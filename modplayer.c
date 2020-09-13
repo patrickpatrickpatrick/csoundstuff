c0f8090961e58dbed3787527f60ba8c2f5abb325
Patrick Cartlidge <patrickcartlidge@gmail.com>
2020-09-13 17:55:51 +0100

    a lotta changes lol

--- modplayer.c
+++ modplayer.c
@@ -1,20 +1,76 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
-#define MOD_NAME_SIZE 20
-#define INSTRUMENT_SIZE 30
-#define NUMBER_OF_INSTRUMENTS 31
-#define PATTERN_TABLE_SIZE 128
-#define BYTE 1
-#define WORD 2
-#define NUMBER_OF_PATTERNS 64
-#define PATTERN_LENGTH 64
-#define NOTE_LENGTH 4
+#include <math.h>
+// i dunno http://web.mit.edu/carrien/Public/speechlab/marc_code/ADAPT_VC/rtaudio/doc/html/index.html
+// use this instead of the other one see if it's easier lol
+
+#define SAMPLE_RATE 		  (8363)
+#define FRAMES_PER_BUFFER 	  (1024)
+
+#define MOD_NAME_SIZE 		  (20)
+#define INSTRUMENT_SIZE 	  (30)
+
+#define INSTRUMENT_NAME_SIZE  (22)
+#define MAX_SAMPLE_SIZE 	  (131070)
+
+#define NUMBER_OF_INSTRUMENTS (31)
+#define PATTERN_TABLE_SIZE 	  (128)
+#define BYTE 				  (1)
+#define WORD 				  (2)
+#define NUMBER_OF_PATTERNS    (64)
+#define PATTERN_LENGTH 		  (64)
+#define PATTERN_SIZE 		  (1024)
+#define NOTE_LENGTH 		  (4)
+#define NUMBER_OF_SAMPLES 	  (30)
 
 int current_position = 0;
+int8_t *mod_file;
 
-void readFileBuffer( uint8_t **buffer_to,  uint8_t **pointer, int pointer_offset, int buffer_size) {
-	*buffer_to = ( uint8_t *) malloc(sizeof( uint8_t)*buffer_size);
+
+// max size for a sample is 131070 bytes
+
+/* yeah so all the samples should be available */
+/* and all the sample lengths i guess */
+// typedef struct
+// {
+// 	int8_t *samples[NUMBER_OF_SAMPLES];
+// } paTestData;
+typedef struct instrument{
+	int8_t *name;
+	int8_t *length;
+	int8_t *finetune;
+	int8_t *volume;
+	int8_t *repeat_offset;
+	int8_t *repeat_length;
+	int8_t *data;
+} instrument;
+
+// readFileBuffer(&(instruments[i].name), &mod_file, current_position, INSTRUMENT_NAME_SIZE);
+// readFileBuffer(&(instruments[i].length), &mod_file, current_position, WORD);
+// readFileBuffer(&(instruments[i].finetune), &mod_file, current_position, BYTE);
+// readFileBuffer(&(instruments[i].volume), &mod_file, current_position, BYTE);
+// readFileBuffer(&(instruments[i].repeat_offset), &mod_file, current_position, WORD);
+// readFileBuffer(&(instruments[i].repeat_length), &mod_file, current_position, WORD);
+
+// typedef struct paTestData
+// {
+// 	instrument (*mod_instruments)[];
+// }
+// paTestData;
+
+static float linearTransformToFloats(float val) {
+  float oldMin = 128;
+  float oldMax = -127;
+  float newMin = -1;
+  float newMax = 1;
+  float oldRange = oldMax - oldMin;
+  float newRange = newMax - newMin;
+  return (((val - oldMin)*newRange) / oldRange) + newMin;
+}
+
+void readFileBuffer( int8_t **buffer_to,  int8_t **pointer, int pointer_offset, int buffer_size) {
+	*buffer_to = ( int8_t *) malloc(sizeof( int8_t)*buffer_size);
 	memcpy(*buffer_to, *pointer + pointer_offset, buffer_size);
 	current_position = pointer_offset + buffer_size;
 }
@@ -22,10 +78,20 @@ void readFileBuffer( uint8_t **buffer_to,  uint8_t **pointer, int pointer_offset
 int main(int argc, char **argv) {
 	FILE *fptr;
 	long file_size;
-	 uint8_t *mod_file, *mod_name,
-		 *instruments[NUMBER_OF_INSTRUMENTS],
-		 *song_length, *pattern_table,
-		 *initials,*patterns[NUMBER_OF_PATTERNS];
+	int8_t *instruments,
+		   *mod_file, *mod_name,
+		   *song_length, *pattern_table,
+		   *initials, *patterns;
+
+
+	// why did i do it like this
+
+	// the whole point of memory addresses is so that you dont need arrays lol
+	// especially because thing is a fixed length...
+	// so u can multiply the length by the index to get the position you need
+	// im so dujb lol
+
+	// struct instrument instruments[NUMBER_OF_INSTRUMENTS];
 
 	if (argc < 2) {
 		printf("There's no file. Closing safely...");
@@ -37,15 +103,19 @@ int main(int argc, char **argv) {
 	file_size = ftell(fptr); // get current file pointer
 	fseek(fptr, 0L, SEEK_SET); // seek to beginning of file again
 
-	mod_file = (uint8_t *) malloc(sizeof(uint8_t)*file_size);
+	mod_file = (int8_t *) malloc(sizeof(int8_t)*file_size);
 	fread(mod_file, sizeof(*mod_file), file_size, fptr);
 
 	fclose(fptr);
 
 	readFileBuffer(&mod_name, &mod_file, current_position, MOD_NAME_SIZE);
 
-	for (int i = 0; i < NUMBER_OF_INSTRUMENTS; i++) {
-		readFileBuffer(&instruments[i], &mod_file, current_position, INSTRUMENT_SIZE);		
+	instruments = (int8_t *) malloc(sizeof(int8_t) * (NUMBER_OF_INSTRUMENTS * (INSTRUMENT_SIZE + MAX_SAMPLE_SIZE)));
+
+	for(int j = 0; j < NUMBER_OF_INSTRUMENTS; j++) {
+		int current_instrument = ((INSTRUMENT_SIZE + MAX_SAMPLE_SIZE) * j);
+		memcpy(instruments + current_instrument, mod_file + current_position, INSTRUMENT_SIZE);
+		current_position = current_position + INSTRUMENT_SIZE;
 	}
 
 	readFileBuffer(&song_length, &mod_file, current_position, BYTE);
@@ -57,32 +127,55 @@ int main(int argc, char **argv) {
 
 	readFileBuffer(&initials, &mod_file, current_position, WORD*2);
 
-	// for (int j = 0; j < PATTERN_LENGTH; j++) {
-	// 	readFileBuffer(&initials, &mod_file, current_position, WORD*2);
-	// }
+	int actual_number_of_patterns = -1;
 
-	for(int k = 0; k < NUMBER_OF_PATTERNS; k++) {
-		readFileBuffer(&patterns[k], &mod_file, current_position, PATTERN_LENGTH);
+	for (int x = 0; x < PATTERN_TABLE_SIZE; x++) {
+		if (pattern_table[x] > actual_number_of_patterns) {
+			actual_number_of_patterns = pattern_table[x];
+		}
 	}
 
+	readFileBuffer(&patterns, &mod_file, current_position, PATTERN_SIZE*actual_number_of_patterns);
 
 	for(int j = 0; j < NUMBER_OF_INSTRUMENTS; j++) {
-		uint8_t *x;
-		// x = &instruments[j][0];
-		x = ( uint8_t *)malloc(sizeof( uint8_t)*WORD);
-		memcpy(x, &instruments[j], WORD);
+		int current_instrument = ((INSTRUMENT_SIZE + MAX_SAMPLE_SIZE) * j);
+		int length_position = current_instrument + INSTRUMENT_NAME_SIZE;
+		int length = (*(instruments + length_position) << 8 | *(instruments + length_position + BYTE)) * 2;
+		memcpy(
+			instruments + current_instrument + INSTRUMENT_SIZE,
+			mod_file + current_position,
+			length
+		);
+		current_position = current_position + length;
+		// (all_instruments->length[0] << 8 | all_instruments->length[1]) * 2
+		// need to calculate length of each sample from the data stored at the pointer
+		// and only incremenent the current position by that much
 
-
-		// x += 23;
-		printf("%d\n", x[0]);
-		printf("%d\n", x[1]);
-		// printf("%d\n", x[0] << 8 | x[1]);
-		printf("%s\n", instruments[j]);
+		// then also need to copy the data into the memory space allocated to each
+		// instrument after
 	}
+
+	// readFileBuffer(&patterns)
+
+	// for(int k = 0; k < actual_number_of_patterns + 1; k++) {
+	// 	readFileBuffer(&patterns[k], &mod_file, current_position, PATTERN_SIZE);
+	// }
+
+	// need to malloc number of instruements * (size of intrument header + size of insutr ment
+	// instruments = (int8_t *) malloc(sizeof(int8_t) * (NUMBER_OF_INSTRUMENTS * (INSTRUMENT_SIZE + MAX_SAMPLE_SIZE)));
+
+	// for(int j = 0; j < NUMBER_OF_INSTRUMENTS; j++) {
+	// 	memcpy(instruments, mod_file + current_position, INSTRUMENT_SIZE);
+	// 	current_position + MAX_SAMPLE_SIZE;
+	// 	// memcpy(*buffer_to, *pointer + pointer_offset, buffer_size);
+	// 	// current_position = pointer_offset + buffer_size;
+	// 	// readFileBuffer(&instruments, &mod_file, current_position, ((*(instruments[j].length)) << 8 | (*(instruments[j].length+1))) * 2);
+	// }
+
+	// hhm these arent the same but rest of file could just be rash
+
 	//aaa need to do hard stuff now lol
+	printf("%d\n", current_position);
+	printf("%ld\n", file_size);
 
-	printf("%s\n", mod_name);
-	// printf("%s\n", instrument);
-
-	return 0;
 }
